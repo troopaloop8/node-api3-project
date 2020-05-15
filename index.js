06c2d74562df84a727613f6c52ba4323def8a2e3
Troopy <40128411+troopaloop8@users.noreply.github.com>
2020-05-15 10:15:32 +0900

    added dotenv for heroku deployment

--- index.js
+++ index.js
@@ -1,9 +1,11 @@
 // code away!
+require('dotenv').config();
 const server = require('./server.js');
 
+const port = process.env.PORT || 5000;
 
-server.listen(6996, () => {
-  console.log("\n*** Server Running on http://localhost:6996 ***\n");
+server.listen(port, () => {
+  console.log(`\n*** Server Running on http://localhost:${port} ***\n`);
 });
 
 
